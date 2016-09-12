eeb4decb0fb83c7ca1c6a7d7575abbbd0eedb54f
mahendra Hirapra <hir.mahendra@yahoo.com>
2016-09-12 09:44:01 +0530

    add namesapce

--- Organization/HotelCommerce.Core.Infrastructure.DependecyResolution/Packages/CommandsPackage.cs
+++ Organization/HotelCommerce.Core.Infrastructure.DependecyResolution/Packages/CommandsPackage.cs
@@ -1,12 +1,12 @@
-﻿using Hotel720.Platform.Infrastructure;
-
 namespace HotelCommerce.Core.Infrastructure.DependecyResolution.Packages
 {
+	using Hotel720.Platform.Infrastructure;
+	
 	public class CommandsPackage : IPackage<SimpleInjector.Container>
-    {
+        {
 		public void RegisterServices(SimpleInjector.Container container)
 		{
 
 		}
-    }
+        }
 }
