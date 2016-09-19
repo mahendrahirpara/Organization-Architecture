cfa5b3c57f91bd19332813d350c7bdfe8e0fe07c
Mahendra Hirapra <hir.mahendra@yahoo.com>
2016-09-19 21:28:47 +0530

    core datacontext changes

---
+++ Organization/HotelCommerce.Core.Infrastructure.DependecyResolution/Packages/CoreDataContextPackage.cs
@@ -0,0 +1,16 @@
+﻿using Hotel720.Platform.Infrastructure;
+using Hotel720.Platform.Infrastructure.Data;
+using HotelCommerce.Core.Infrastructure.Data;
+using MongoDB.Driver;
+
+namespace HotelCommerce.Core.Infrastructure.DependecyResolution.Packages
+{
+	public sealed class CoreDataContextPackage : IPackage<SimpleInjector.Container>
+	{
+		public void RegisterServices(SimpleInjector.Container container)
+		{
+			container.RegisterSingleton<IDataContextFactory<IMongoDatabase>, CoreDataContext>();
+			//container.RegisterSingleton<IUnitOfWorkFactory, UnitOfWorkFactory>();
+		}
+	}
+}
